351d3dd343f4405b0135e8ee100428fcb337065c
Tim Andersson <tim.andersson@tietoevry.com>
2022-03-03 12:20:55 +0100

    Moved dir

--- random/exportUserGroups.ps1
+++
@@ -1,3 +0,0 @@
-﻿Write-Host "Exportera grupper för en användare."
-$user = Read-Host -Prompt "Användarnamn"
-Get-ADPrincipalGroupMembership $user | Select-Object name | export-csv "grupper.csv" -Delimiter ";" -NoTypeInformation -Encoding UTF8
